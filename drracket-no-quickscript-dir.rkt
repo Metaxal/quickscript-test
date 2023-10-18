7daaf7eb39b7a728f597829ae05688f527f9ad4e
Metaxal <laurent.orseau@gmail.com>
2023-10-18 21:54:26 +0100

    fix missing script dir in library

--- drracket-no-quickscript-dir.rkt
+++ drracket-no-quickscript-dir.rkt
@@ -2,13 +2,19 @@
 
 (require tests/drracket/private/drracket-test-util
          quickscript/base
+         (prefix-in lib: quickscript/library)
          rackunit
          "base.rkt")
 
 ;; Test if DrRacket starts correctly when the quickscript directory does not initially exist,
 ;; which is the setup of a first install.
+;; DrDr should do that already though.
 
 (remove-quickscript-dir-until-exit!)
+
+;; Make sure the user script library is included, otherwise some tests will fail, like
+;; when creating a new script.
+(lib:add-third-party-script-directory! user-script-dir)
 
 (define prefs `(,tools-prefs))
 
